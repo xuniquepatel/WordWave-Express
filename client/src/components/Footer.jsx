93e89657e638c9f7def487bbcc3fc61391f4252c
007unique <patelunique1@gmail.com>
2023-08-05 13:50:12 +0530

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.jpeg";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made by <b>uniqueWorld33</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
